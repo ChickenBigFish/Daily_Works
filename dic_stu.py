99f27ca4ff015a62fdfc2e75cb5057c59514c7a0
chenyang!!!! <51943538+ChickenBigFish@users.noreply.github.com>
2019-11-19 23:40:40 +0000

    Delete dic_stu.py

--- dic_stu.py
+++
@@ -1,7 +0,0 @@
-dic_student = {}
-for i in range(5):
-    name = input("请输入姓名：")
-    age = input("请输入年龄")
-    dic_student[name] = age
-for k, v in dic_student.items():
-    print("{0:{2}<10}{1}".format(k, v, chr(12288)))
